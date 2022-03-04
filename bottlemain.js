48f51f89afb83e5ecae9a8b40e41bbd71a59353c
Rudra-00 <82320714+Rudra-00@users.noreply.github.com>
2022-03-04 16:05:18 +0530

    Add files via upload

---
+++ bottlemain.js
@@ -0,0 +1,49 @@
+
+ objectDetector= "";
+
+ img = "";
+ objects = [];
+ status = "";
+ 
+ function preload(){
+   img = loadImage('Bottle.jpg');
+ }
+ 
+ 
+ function setup() {
+   canvas = createCanvas(640, 640);
+   canvas.center();
+   objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+   document.getElementById("status").innerHTML = "Status : Detecting Objects";
+ }
+ 
+ function modelLoaded() {
+   console.log("Model Loaded!")
+   status = true;
+   objectDetector.detect(img, gotResult);
+ }
+ 
+ function gotResult(error, results) {
+   if (error) {
+     console.log(error);
+   }
+   console.log(results);
+   objects = results;
+ }
+ 
+ 
+ function draw() {
+   if (status != undefined) {
+    image(img, 0, 0, 640, 640);
+     for (var i = 0; i < objects.length; i++) {
+       document.getElementById("status").innerHTML = "Status : Objects Detected";
+ 
+       fill(255, 0, 0);
+       percent = floor(objects[i].confidence * 100);
+       text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+       noFill();
+       stroke(255, 0, 0);
+       rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+     }
+   }
+ }
---
+++ main.js
@@ -0,0 +1,15 @@
+function dog(){
+    window.location="dogindex.html";
+}
+function Bottle(){
+    window.location="bottleindex.html";
+}
+function baseballandballbat(){
+    window.location="baseindex.html";
+}
+function scissors(){
+    window.location="scissorsindex.html";
+}
+function bed(){
+    window.location="bedindex.html";
+}
